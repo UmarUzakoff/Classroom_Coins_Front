b74e884838404bf2678629cd84ca9846c45b510b
UzakoffUmar <uzakovumar338@gmail.com>
2023-09-23 18:34:25 +0500

    bugs fixed

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import { Flip } from "react-reveal";
 
 export const Loader = ({enteringText}) => {
   return (
-    <div className="container flex justify-center flex-col items-center h-[65vh]">
+    <div className="container flex justify-center flex-col items-center h-[70vh]">
       <div className="loader"></div>
       <Flip left text>
         <h1 className="mt-5 animate-text bg-gradient-to-r from-gray-700 via-gray-400 to-orange bg-clip-text text-transparent text-3xl xl:text-5xl font-rem font-black text-center">
