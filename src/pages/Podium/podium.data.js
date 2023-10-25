4c41de510c82e7a22798a8708ba0785cbe421e39
UzakoffUmar <uzakovumar338@gmail.com>
2023-10-25 18:17:32 +0500

    new memory added

--- src/pages/Podium/podium.data.js
+++ src/pages/Podium/podium.data.js
@@ -19,7 +19,7 @@ import memories1_gw15_month2 from "../../images/memories/gw-15/month2/memories1.
 import shoxsulton_2_certificate from "../../images/certificates/Shoxsulton_2.png";
 import alibek_certificate from "../../images/certificates/Alibek.png";
 import memory_hackathon from "../../images/memories/gw-13/month3/hackaton.jpg";
-
+import memory_3month_gw13 from "../../images/memories/gw-13/month3/memory.jpg";
 
 const winnersData = [
   {
@@ -35,21 +35,24 @@ const winnersData = [
             certificate: sevinch_certificate,
             coins: 215,
             rank: 1,
-            avatar: "https://i.pinimg.com/564x/f7/0d/6c/f70d6c4271126184ce5cce40a53611a9.jpg"
+            avatar:
+              "https://i.pinimg.com/564x/f7/0d/6c/f70d6c4271126184ce5cce40a53611a9.jpg",
           },
           {
             fullName: "Javohir Murodov",
             certificate: javohir_certificate,
             coins: 214,
             rank: 2,
-            avatar: "https://i.pinimg.com/474x/88/b3/e4/88b3e42479fb2d3440681af9ef9ab95b.jpg"
+            avatar:
+              "https://i.pinimg.com/474x/88/b3/e4/88b3e42479fb2d3440681af9ef9ab95b.jpg",
           },
           {
             fullName: "Fayoziddin Appakxodjayev",
             certificate: fayoziddin_certificate,
             coins: 186,
             rank: 3,
-            avatar: "https://img.freepik.com/premium-photo/very-cute-kid-caracter-animation-pixar-style_950002-73964.jpg"
+            avatar:
+              "https://img.freepik.com/premium-photo/very-cute-kid-caracter-animation-pixar-style_950002-73964.jpg",
           },
         ],
       },
@@ -62,48 +65,54 @@ const winnersData = [
             certificate: sevinch_certificate,
             coins: 269,
             rank: 1,
-            avatar: "https://i.pinimg.com/564x/f7/0d/6c/f70d6c4271126184ce5cce40a53611a9.jpg"
+            avatar:
+              "https://i.pinimg.com/564x/f7/0d/6c/f70d6c4271126184ce5cce40a53611a9.jpg",
           },
           {
             fullName: "Javohir Murodov",
             certificate: javohir_certificate,
             coins: 225,
             rank: 2,
-            avatar: "https://i.pinimg.com/474x/88/b3/e4/88b3e42479fb2d3440681af9ef9ab95b.jpg"
+            avatar:
+              "https://i.pinimg.com/474x/88/b3/e4/88b3e42479fb2d3440681af9ef9ab95b.jpg",
           },
           {
             fullName: "Shoxsulton Isoqov",
             certificate: shoxsulton_certificate,
             coins: 192,
             rank: 3,
-            avatar: "https://i.pinimg.com/564x/0e/87/6e/0e876e130d0844af9105f05398a28abc.jpg"
+            avatar:
+              "https://i.pinimg.com/564x/0e/87/6e/0e876e130d0844af9105f05398a28abc.jpg",
           },
         ],
       },
       {
         monthNum: 3,
-        memories: [memory_hackathon],
+        memories: [memory_hackathon, memory_3month_gw13],
         topStudents: [
           {
             fullName: "Sevinch Sayfutdinova",
             certificate: sevinch_certificate,
             coins: 270,
             rank: 1,
-            avatar: "https://i.pinimg.com/564x/f7/0d/6c/f70d6c4271126184ce5cce40a53611a9.jpg"
+            avatar:
+              "https://i.pinimg.com/564x/f7/0d/6c/f70d6c4271126184ce5cce40a53611a9.jpg",
           },
           {
             fullName: "Shoxsulton Isoqov",
             certificate: shoxsulton_2_certificate,
             coins: 185,
             rank: 2,
-            avatar: "https://i.pinimg.com/564x/0e/87/6e/0e876e130d0844af9105f05398a28abc.jpg"
+            avatar:
+              "https://i.pinimg.com/564x/0e/87/6e/0e876e130d0844af9105f05398a28abc.jpg",
           },
           {
             fullName: "Alibek Akromov",
             certificate: alibek_certificate,
             coins: 174,
             rank: 3,
-            avatar: "https://whatsondisneyplus.com/wp-content/uploads/2021/06/luca-avatar-WODP.png"
+            avatar:
+              "https://whatsondisneyplus.com/wp-content/uploads/2021/06/luca-avatar-WODP.png",
           },
         ],
       },
@@ -122,21 +131,24 @@ const winnersData = [
             certificate: komoliddin_certificate,
             coins: 310,
             rank: 1,
-            avatar: "https://i.pinimg.com/564x/f5/bc/31/f5bc31d68868b93ff33fe444babb3eef.jpg"
+            avatar:
+              "https://i.pinimg.com/564x/f5/bc/31/f5bc31d68868b93ff33fe444babb3eef.jpg",
           },
           {
             fullName: "Shoxdiyor Shirinboyev",
             certificate: shoxdiyor_certificate,
             coins: 286,
             rank: 2,
-            avatar: "https://i.pinimg.com/564x/0b/b4/10/0bb410482ac53dbfd80d571023af8f60.jpg"
+            avatar:
+              "https://i.pinimg.com/564x/0b/b4/10/0bb410482ac53dbfd80d571023af8f60.jpg",
           },
           {
             fullName: "MuhammadAmin Botirov",
             certificate: muhammadamin_certificate,
             coins: 197,
             rank: 3,
-            avatar: "https://i.pinimg.com/564x/ef/91/b1/ef91b151821245ed97c4bcee71687a6f.jpg"
+            avatar:
+              "https://i.pinimg.com/564x/ef/91/b1/ef91b151821245ed97c4bcee71687a6f.jpg",
           },
         ],
       },
@@ -149,21 +161,24 @@ const winnersData = [
             certificate: muhammadamin_gold,
             coins: 311,
             rank: 1,
-            avatar: "https://i.pinimg.com/564x/ef/91/b1/ef91b151821245ed97c4bcee71687a6f.jpg"
+            avatar:
+              "https://i.pinimg.com/564x/ef/91/b1/ef91b151821245ed97c4bcee71687a6f.jpg",
           },
           {
             fullName: "Abdusolih Xalilov",
             certificate: abdusolih_silver,
             coins: 251,
             rank: 2,
-            avatar: "https://i.pinimg.com/564x/e2/d3/a2/e2d3a2616bdad7166567458c319a2146.jpg"
+            avatar:
+              "https://i.pinimg.com/564x/e2/d3/a2/e2d3a2616bdad7166567458c319a2146.jpg",
           },
           {
             fullName: "Komoliddin Zayniddinov",
             certificate: komoliddin_bronze,
             coins: 200,
             rank: 3,
-            avatar: "https://i.pinimg.com/564x/f5/bc/31/f5bc31d68868b93ff33fe444babb3eef.jpg"
+            avatar:
+              "https://i.pinimg.com/564x/f5/bc/31/f5bc31d68868b93ff33fe444babb3eef.jpg",
           },
         ],
       },
